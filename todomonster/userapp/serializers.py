61adb4a7a9a6c7171732e628e3b408680e14b020
Nikolos123 <Isponec2>
2022-02-05 12:05:44 +0300

    homework_1

--- todomonster/userapp/serializers.py
+++ todomonster/userapp/serializers.py
@@ -1,8 +1,8 @@
-from rest_framework.serializers import HyperlinkedModelSerializer
+from rest_framework.serializers import ModelSerializer
 from .models import User
 
 
-class UserSerializer(HyperlinkedModelSerializer):
+class UserSerializer(ModelSerializer):
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email')
